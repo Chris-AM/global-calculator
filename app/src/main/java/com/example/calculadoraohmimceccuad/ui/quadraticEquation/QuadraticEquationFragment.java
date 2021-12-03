3d82dd89d4785b1d294a5c0701a70f951ae1e0ff
chris.am <c.aranguizm@outlook.com>
2021-12-03 17:38:11 -0300

    declaring view variables

--- app/src/main/java/com/example/calculadoraohmimceccuad/ui/quadraticEquation/QuadraticEquationFragment.java
+++ app/src/main/java/com/example/calculadoraohmimceccuad/ui/quadraticEquation/QuadraticEquationFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +13,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.calculadoraohmimceccuad.R;
 import com.example.calculadoraohmimceccuad.databinding.FragmentQuadraticEquationBinding;
 
 public class QuadraticEquationFragment extends Fragment {
@@ -24,7 +26,12 @@ public class QuadraticEquationFragment extends Fragment {
         binding = FragmentQuadraticEquationBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-
+        final TextView a_input = root.findViewById(R.id.txt_a_input);
+        final TextView b_input = root.findViewById(R.id.txt_b_input);
+        final TextView c_input = root.findViewById(R.id.txt_c_input);
+        final Button calculate = root.findViewById(R.id.btn_calc_quadratic_equation);
+        
+        
         return root;
     }
 
