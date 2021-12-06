b548808ab2a384e5b9ada90c09f52fa949893373
chris.am <c.aranguizm@outlook.com>
2021-12-06 12:17:15 -0300

    app now can calculate quadratic equations and find if the equations has one or two solutions and if it has real or imaginary solutions

--- app/src/main/java/com/example/calculadoraohmimceccuad/ui/quadraticEquation/QuadraticEquationFragment.java
+++ app/src/main/java/com/example/calculadoraohmimceccuad/ui/quadraticEquation/QuadraticEquationFragment.java
@@ -1,5 +1,7 @@
 package com.example.calculadoraohmimceccuad.ui.quadraticEquation;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +20,8 @@ import com.example.calculadoraohmimceccuad.databinding.FragmentQuadraticEquation
 
 public class QuadraticEquationFragment extends Fragment {
 
+    Double a, b, c, x1, x2, delta, imaginary, real,
+            xFinal, roundedX1, roundedX2, roundedImaginary, roundedReal, roundedFinal;
     private FragmentQuadraticEquationBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -30,8 +34,91 @@ public class QuadraticEquationFragment extends Fragment {
         final TextView b_input = root.findViewById(R.id.txt_b_input);
         final TextView c_input = root.findViewById(R.id.txt_c_input);
         final Button calculate = root.findViewById(R.id.btn_calc_quadratic_equation);
-        
-        
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String convertedA = a_input.getText().toString();
+                String convertedB = b_input.getText().toString();
+                String convertedC = c_input.getText().toString();
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+                try {
+                    a = Double.parseDouble(convertedA);
+                    if (a == 0) {
+                        builder.setTitle("Error");
+                        builder.setMessage("<<a>> debe ser distinto de 0");
+                        builder.setNegativeButton("Entendido", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        builder.show();
+                    } else {
+                        b = Double.parseDouble(convertedB);
+                        c = Double.parseDouble(convertedC);
+                        delta = (b * b) - (4 * a * c);
+                        if (delta > 0) {
+                            x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                            x2 = (-b - Math.sqrt(delta)) / (2 * a);
+                            roundedX1 = Math.round(x1 * 100.0) / 100.0;
+                            roundedX2 = Math.round(x2 * 100.0) / 100.0;
+                            builder.setTitle("Resultado");
+                            builder.setMessage("La equación tiene dos soluciones:\ndelta ===> "
+                                    + delta + "\n x1 ===> " + roundedX1 + "\nx2 ===> " + roundedX2);
+                            builder.setNegativeButton("Entendido", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                            builder.show();
+                        } else if (delta == 0) {
+                            xFinal = -b / (2 * a);
+                            roundedFinal = Math.round(xFinal * 100.0) / 100.0;
+                            builder.setTitle("Resultado");
+                            builder.setMessage("La equación tiene solo una solución:\ndelta ===> "
+                                    + delta + "\n x ===> " + roundedFinal);
+                            builder.setNegativeButton("Entendido", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                            builder.show();
+                        } else if (delta < 0) {
+                            imaginary = Math.sqrt(-delta) / (2 * a);
+                            real = -b / (2 * a);
+                            roundedImaginary = Math.round(imaginary * 100.0) / 100.0;
+                            roundedReal = Math.round(real * 100.0) / 100.0;
+                            builder.setTitle("Resultado");
+                            builder.setMessage("La equación tiene una solución compleja y otra real:\ndelta ===> "
+                                    + delta + "\nImaginaria ===> " + roundedImaginary + "i\nReal ===> " + roundedReal);
+                            builder.setNegativeButton("Entendido", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                            builder.show();
+                        }
+                    }
+                } catch (Exception e) {
+                    AlertDialog.Builder error = new AlertDialog.Builder(getContext());
+                    error.setTitle("Error");
+                    error.setMessage("Por favor, ingresar todos los valores");
+                    error.setNegativeButton("Entendido", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    error.show();
+                }
+
+            }
+        });
         return root;
     }
 
